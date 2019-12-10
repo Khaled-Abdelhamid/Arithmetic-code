8f8bf39519b624d923fba589a4d8162d73aa6c73
Khaled Abdelhamid <khaledusama1998@gmail.com>
2019-12-10 11:22:03 +0200

    reference the source

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from helpers import *
 import numpy as np
 
 # in this code we will use integer based arithmetic code insted of the infinite prescision version 
+# this version is based upon this youtube tutorial : https://www.youtube.com/watch?v=ouYV3rBtrTI&list=PLE125425EC837021F&index=41
+
 
 # creating arbitrary stream to test on 
 stream = [1,2,3,4,5,7,85,4,5,7,-5,516,8,1,68,1,313,-5,18,9,7,351,32,15,6,84,13,20,56,85,4,55,6,7,85,4,-5,516,8,1,68,1,313,-5,18,9,7,351,32,15,6,5,5,7,4,5,6,7,86,7,8,8,7,6,85,7,4,5,6,7]
