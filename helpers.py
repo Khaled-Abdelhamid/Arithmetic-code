0fb865171de6b1cdcf0a8cafdb272304b2f50a5e
Khaled Abdelhamid <khaledusama1998@gmail.com>
2019-12-10 11:20:44 +0200

    full commenting of the code

--- helpers.py
+++ helpers.py
@@ -1,66 +1,68 @@
 import numpy as np
 def getfreqs(stream):
     """
-    This function Takes a stream as an input. it gets the number of occurnces of each symbol
+    This function Takes a stream as an input. it gets the number of occurrences of each symbol
     in that stream,  store it in a Dictionary containing every symbol in the stream .
-    and returns the Arithmetic code of that stream
     
     Arguments:
-        stream {list} -- [description]
+        stream {list} -- [the input stream we want to compress]
     Returns:    
-        Dict {dictionary} -- [description]
+        Dict {dictionary} -- [contains the number of occurrences of each symbol in the stream,referenced by the name of the symbol]
     """
 
     symbols = set(stream)   #get the unique elements
     L = len(stream)
     Dict = {}          #store each symbol with its corresponding probability
-    #calculate the probability of each symbol in the sream 
+    
+    #calculate the probability of each symbol in the stream 
     for s in symbols:
         freq = stream.count(s)
         Dict[s] = freq
     return Dict
 
 def Cumfreq(symbol, dictionary):
-  
-  '''This Function Takes as  inputs a symbol and a dictionary containing
+  """
+  This Function Takes as  inputs a symbol and a dictionary containing
   all the symbols that exists in our stream and their frequencies 
-  and returns the cumulative freuency starting from the very 
-  beginning of the Dictionary untill that Symbol.
-  '''
-  P = 0.0
+  and returns the cumulative frequency starting from the very 
+  beginning of the Dictionary until that Symbol.
+  
+  Arguments:
+      symbol {[type]} -- [the symbol we want to get the cumulative frequency for]
+      dictionary {[type]} -- [the dictionary that contains the frequency of all symbols]
+  Returns:
+      p int -- [the upper bound ,which is the sum of all frequencies from the beginning of the dictionary till the specified symbol]
+  """
+
+  P = 0
   for sym in dictionary:
       P += dictionary[sym]
       if sym == symbol:
         break
   return P
 
-def CumulativeDict(Dict, start = 0):
+
+def Arithmetic_encode (stream,precision=32):
+    """
+    The encoder function for the arithmetic code,all of its operations are supposed to be on integers
     
-  ''' This Function Takes as an input a dictionary of symbols its probability,
-      and return the Cumulative probability for each symbol. we use it in defining a decimal region for each symbol.
-      another parameter can be passed to the function, the starting point of the whole symbols, which is by default zero.
-  '''
-  CumDict = {}
-  
-  for symbol in Dict:
-    CP = start
-    for sym in Dict:
-      CP += Dict[sym]
-      if sym == symbol:
-        break
-    CumDict[symbol] = CP
-  return CumDict
-
-
-def Arithmatic_encode (stream,precision=32):
-    stream.append('!')
+    Arguments:
+        stream {list} -- [the input stream we want to encode]
+    
+    Keyword Arguments:
+        precision {int} -- [the number of precision bits used in the encoder] (default: {32})
+    
+    Returns:
+        code {list} -- [the binary compressed code of the input stream]
+        dic  {dictionary}  -- [it contains the dictionary used to encode the stream.It includes the number of occurrences of all symbols in the stream] 
+    """
+    stream.append('!')# add the end of file symbol so the that the decoder knows when to stop
 
     StreamSize=len(stream)
 
-    dic=getfreqs(stream) #output the the dictionary that contains the probabilities of  all ymbols
-    syms_no=len(dic)#gets the numbers of symbols
-    print (dic)
-    symbol=0
+    dic=getfreqs(stream) #output the the dictionary that contains the probabilities of  all symbols
+    
+    
     index=0
 
     # those numbers will be used later in the scaling and emitting step of the binary bits
@@ -73,24 +75,20 @@ def Arithmatic_encode (stream,precision=32):
     trails=0 # a variable to trace how deep i entered at the corner case of which the low and high limits are lying in the middle of the range
 
 
-    code = [] # the list that will include the comressed code 
+    code = [] # the list that will include the compressed code 
 
-    # while symbol != '!':
     for symbol in stream:        
         
         freqSym=dic[symbol]          # get the frequency of the symbol
         S_high=Cumfreq(symbol,dic)   # get the higher limit of this symbol
         S_low=S_high-freqSym         # get the lower limit of this symbol
         
-        Range=H-L                    # get the range of the code     
-        # if (Range<(quarter+2)):
-        #     print("precision error ,try to increase the precision")
-        #     break
-        # the rounding happens to gurantee that everything is integer
+        Range=H-L                    # get the range of the code    
+        # rescale the limits to the new symbol,we are using integer division to guarantee that the output is integer 
         H = L +  Range * S_high // StreamSize 
         L = L +  Range * S_low // StreamSize 
         
-        #creating the cases for which we will emmit 0 or 1 to our final code word
+        #creating the cases for which we will emit 0 or 1 to our final code word
         while True: # the first two easy cases , if my range fully falls inside one block
             if H < half : # if the full range falls in the lower half 
                 code.extend([0])
@@ -112,6 +110,7 @@ def Arithmatic_encode (stream,precision=32):
                 H=2*(H-quarter)  
             else:
                 break        
+    # add the last bits before you exit the function
     trails+=1
     if L<=quarter:
         code.extend([0])
@@ -121,13 +120,24 @@ def Arithmatic_encode (stream,precision=32):
         code.extend([0]*trails)
     return code ,dic
 
-def Arithmatic_decode (code,dic,precision=32):
-        
+def Arithmetic_decode (code,dic,precision=32):
+    """The decoder function for the arithmetic code,all of its operations are supposed to be on integers
+    
+    Arguments:
+        code {list} -- [the binary compressed code of the input stream]
+        dic {dictionary} -- [it contains the dictionary used to encode the stream.It includes the number of occurrences of all symbols in the stream]
+    
+    Keyword Arguments:
+        precision {int} -- [the number of precision bits used in the decoder] (default: {32})
+    
+    Returns:
+       message {list} -- [the decoder output.It's supposed to be an identical copy of the input stream]
+    """ 
     code_size=len(code)
     
-    stream_size=sum(dic.values())
-    # those numbers will be used later in the scaling and emitting step of the binary bits
+    stream_size=sum(dic.values())#get the stream size buy summing the number of occurrences of all the symbols in the stream
     
+    # those numbers will be used later in the scaling and emitting step of the binary bits
     full =2**precision
     half=full//2
     quarter=half//2
@@ -135,20 +145,20 @@ def Arithmatic_decode (code,dic,precision=32):
     L=0 # the lower limit of the range
     H=full # the upper limit of the range
 
-    val=0 # a variable to trace how deep i entered at the corner case of which the low and high limits are lying in the middle of the range
-    indx=1
+    val=0 # a variable that will hold the probability which we will iterate to find the symbol using it
+    indx=1 # an index to trace the place of the last bit used to find the code.This  is due to the limited precision we have,as we cannot compute the whole floating number at once
 
     message=[]
 
-    while indx <= precision and indx <= code_size:# first get the exact amount of values that can be held in the precsion available , the rest will be used during the code
+    while indx <= precision and indx <= code_size:# first get the exact amount of values that can be held in the precision available , the rest will be used during the code
         if code[indx-1]==1:
             val=val+2**(precision-indx)
         indx+=1
     flag=1
 
-    while flag:
+    while flag:# the flag will be zero if we found the end of file symbol
          
-        for symbol in dic:  
+        for symbol in dic:  # this loop tries to find the symbol that has the range specified by the variable val 
             
             freqSym=dic[symbol]    # get the frequency of the symbol
             S_high=Cumfreq(symbol,dic)   # get the higher limit of this symbol
@@ -163,7 +173,7 @@ def Arithmatic_decode (code,dic,precision=32):
                 message.extend([symbol])
                 L=L0
                 H=H0 
-                if symbol == '!':
+                if symbol == '!':# this condition should be satisfied at the end of the stream
                     flag=0
                 break    
                     
@@ -182,16 +192,16 @@ def Arithmatic_decode (code,dic,precision=32):
                 if indx<= code_size:
                     val+=code[indx-1]
                     indx+=1 
-            elif L>=quarter and H < 3*quarter:
+            elif L>=quarter and H < 3*quarter: # if the lower limit is in the lower half na d the  upper range is in the upper half,then we will need to expand the range more
                 L=2*(L-quarter)
                 H=2*(H-quarter)
                 val=2*(val-quarter)
                 
-                if indx<= code_size:
+                if indx<= code_size:# increase the precision of pur probability until we finish all the the code bits
                     val+=code[indx-1]
                     indx+=1 
             else:
                 break
     
-    message.pop()        
+    message.pop() # remove the end of stream symbol to get the original stream        
     return message
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from helpers import *
 import numpy as np
 
-# in this code we will use integer based arithmatic code insted of the infinte presciosn version 
+# in this code we will use integer based arithmetic code insted of the infinite prescision version 
+
+# creating arbitrary stream to test on 
 stream = [1,2,3,4,5,7,85,4,5,7,-5,516,8,1,68,1,313,-5,18,9,7,351,32,15,6,84,13,20,56,85,4,55,6,7,85,4,-5,516,8,1,68,1,313,-5,18,9,7,351,32,15,6,5,5,7,4,5,6,7,86,7,8,8,7,6,85,7,4,5,6,7]
-stream=stream*20
+stream=stream*20 # this is just made to increase the size without writing too much :)
 
+ # Unlike the infinite precision case, here we are confined with a finite amount of bits
+ # the number of precision bits that from which we will try to handle the code
 precision=32
-stream_size=len(stream)+1 # add the end of  file symbol
 
-code,dic =Arithmatic_encode(stream,precision)
+# call the encoder function
+code,dic =Arithmetic_encode(stream,precision)
 
 print (code)
-print (len(code))
+print ('number of bits is : ',len(code))
 
-message=Arithmatic_decode (code,dic,precision)
+# call the decoder function
+message=Arithmetic_decode (code,dic,precision)
 
-stream.pop()
+stream.pop()# remove the end of file symbol so that we could compare the stream with the decoded messsage
 
 print(stream)     
 print (message)
